f8519240ac6f8857a48e5ee96bec72ac061a0ee4
Lyrikkse <devil.lord@mail.ru>
2020-07-03 12:54:54 +0700

    lesson2

--- lesson-2/lesson-2.js
+++ lesson-2/lesson-2.js
@@ -6,8 +6,6 @@ JSON.stringify({x:10})
 ProxiedObject.a = 1
 // ProxiedObject: { a: 1, x: 10 }
 
-JSON.stringify({b:{c:{d:2}})
-
 ProxiedObject.b.c.d = 2
 // ProxiedObject: { a: 1, b: { c: { d: 2 } }, x: 10 }
 
