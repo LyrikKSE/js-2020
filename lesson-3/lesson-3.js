15737787d3aff8141ac7054101c3403a67ca520a
Lyrikkse <devil.lord@mail.ru>
2020-07-22 11:21:53 +0700

    3

--- lesson-3/lesson-3.js
+++ lesson-3/lesson-3.js
@@ -12,26 +12,40 @@ const getLazy = (obj) => {
                 switch (prop) {
                     case 'map':
                         return predicate => getLazy({
-                            [Symbol.iterator]() {
-                                return this
-                            },
+                            [Symbol.iterator]() { return this },
                             index: 0,
                             next() {
-                                const {value, done} = iterator.next()
+                                const { value, done } = iterator.next()
                                 if (done) {
-                                    return {done}
-                                } else {
-                                    return {done, value: predicate(value, this.index++)}
+                                    return { done }
+                                }
+
+                                return { done, value: predicate(value, this.index++) }
+                            }
+                        })
+                    case 'filter':
+                        return predicate => getLazy({
+                            [Symbol.iterator]() { return this },
+                            index: 0,
+                            next() {
+                                while (true) {
+                                    let { done, value } = iterator.next();
+
+                                    if (done) {
+                                        return { done };
+                                    }
+
+                                    if (predicate(value, this.index++)) {
+                                        return { done, value };
+                                    }
                                 }
                             }
                         })
                     case 'take':
                         return (count) => getLazy({
-                            [Symbol.iterator]() {
-                                return this
-                            },
+                            [Symbol.iterator]() { return this },
                             next() {
-                                return count-- ? iterator.next() : {done: true}
+                                return count-- ? iterator.next() : { done: true }
                             }
                         })
                     default:
@@ -40,38 +54,12 @@ const getLazy = (obj) => {
             }
         }
     )
+
 }
 
 const list = [1, 2, 3, 4, 5]
 const lazyIterator = getLazy(list)
-    .map(x => {
-        console.log('map 1');
-        return x + 10
-    })
-    .map(x => console.log('map 2') || (x + 1))
-    .map((x, i) => {
-        if (i == 3) {
-            throw 'Oops!!'
-        } else return x
-    })
-    .take(3)
-    .map(x => (console.log('map 3'), x ** 2))
+    .map(x => { console.log('map 1'); return x + 10 })
+    .filter((value, key) => value > 12)
 
 console.log(...lazyIterator)
-
-function* generator() {
-    yield 1
-    yield 2
-    yield 3
-}
-
-const generatorObject = generator()
-
-
-const endlessIterator = {
-    value: 0,
-    next() {
-        return {value: this.value++, done: false}
-    }
-}
-console.log(...getLazy(endlessIterator).map(x => x ** 2).take(10))
